223762b2a7b4b12216b5eb4a4d727581e10fa7c9
Aes Saputra <aessaputra@yahoo.com>
2025-12-06 01:04:35 +0700

    refactor: translate login form UI text to Indonesian.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,12 +16,12 @@
                         Password
                         @if (Route::has('password.request'))
                             <span class="form-label-description">
-                                <a href="{{ route('password.request') }}">I forgot password</a>
+                                <a href="{{ route('password.request') }}">Lupa password?</a>
                             </span>
                         @endif
                     </label>
                     <div class="input-group input-group-flat">
-                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Your password" required autocomplete="current-password">
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Masukkan password" required autocomplete="current-password">
                         <span class="input-group-text">
                             <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
@@ -35,11 +35,11 @@
                 <div class="mb-2">
                     <label class="form-check">
                         <input type="checkbox" class="form-check-input" name="remember">
-                        <span class="form-check-label">Remember me on this device</span>
+                        <span class="form-check-label">Ingat saya di perangkat ini</span>
                     </label>
                 </div>
                 <div class="form-footer">
-                    <button type="submit" class="btn btn-primary w-100">Sign in</button>
+                    <button type="submit" class="btn btn-primary w-100">Masuk</button>
                 </div>
             </form>
         </div>
